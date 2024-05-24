5d6a491ff8b8e9792aa573dde60962a59aab5b17
Mark Sinakaev <Sinakaev@lanit.ru>
2024-05-24 13:09:47 +0300

    Add Redux Tk

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App";
+import { store } from "./store";
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
